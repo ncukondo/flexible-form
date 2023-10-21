4acbee2ac19060601fff51df78566e3211464220
Takeshi Kondo <ncukondo@gmail.com>
2023-10-21 03:52:29 +0000

    add corbado login flow and update nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
-import Image from "next/image";
+"use client";
+
+import "@corbado/webcomponent/pkg/auth_cui.css";
+import { useEffect } from "react";
 
 export default function Home() {
-  return <main></main>;
+  useEffect(() => {
+    // クライアントのみで実行
+    import("@corbado/webcomponent")
+      .then(resp => {
+        console.log(resp);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className="grid justify-center items-center min-h-[100dvh]">
+      <corbado-auth project-id={process.env.NEXT_PUBLIC_CORBADO_PROJECT_ID} conditional="yes">
+        <input
+          name="username"
+          id="corbado-username"
+          data-input="username"
+          required
+          autoComplete="webauthn"
+        />
+      </corbado-auth>
+    </div>
+  );
 }
