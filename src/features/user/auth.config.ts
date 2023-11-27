eb0fbb892b838b25cc1c824efe403916e9e0340c
Takeshi Kondo <ncukondo@gmail.com>
2023-11-27 01:57:26 +0000

    update docker-compose to support shadow db

---
+++ src/features/user/auth.config.ts
@@ -0,0 +1,27 @@
+import type { NextAuthConfig } from "next-auth"
+import Email from "next-auth/providers/email"
+
+export default {
+  providers: [
+    Email({
+      server: {
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl
+    }
+  }
+} satisfies NextAuthConfig
