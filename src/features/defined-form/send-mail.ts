a152dbd65bf9b095bb8d8ab42db495ded1ac7424
Takeshi Kondo <ncukondo@gmail.com>
2025-02-25 07:44:59 +0000

    fix: avoid error by url auto processing in sendgrid

--- src/features/defined-form/send-mail.ts
+++ src/features/defined-form/send-mail.ts
@@ -1,12 +1,17 @@
 import sgMail from "@sendgrid/mail";
 
+const avoidErrorByUrlAutoProcess = (text: string) => {
+  const regex = /(https?:\/\/[\w!\?/\+\-_~=;\.,\*&@#\$%\(\)'\[\]]+)"/g;
+  return text.replaceAll(regex, "$1 \"");
+}
+
 const sendSystemMessageMail = async (mailTo: string, body: string, title = "") => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY ?? "");
   const msg = {
     to: mailTo,
     from: process.env.SENDGRID_MAIL_FROM ?? "",
     subject: title || "System Mail from THERS",
-    text: body,
+    text: avoidErrorByUrlAutoProcess(body),
   };
   console.log("mail", msg);
   try {
