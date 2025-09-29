89d050fc9ea6b324b3a32021bbee7956e68b8fa8
Takeshi Kondo <ncukondo@gmail.com>
2025-09-29 08:11:01 +0000

    change smtp server from SendGrid to Resend

--- src/features/defined-form/send-mail.ts
+++ src/features/defined-form/send-mail.ts
@@ -1,17 +1,28 @@
-import sgMail from "@sendgrid/mail";
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD
+  },
+  pool: true,
+});
 
 
 const sendSystemMessageMail = async (mailTo: string, body: string, title = "") => {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY ?? "");
   const msg = {
     to: mailTo,
-    from: process.env.SENDGRID_MAIL_FROM ?? "",
+    from: process.env.EMAIL_FROM ?? "",
     subject: title || "System Mail from THERS",
     text: body,
   };
   console.log("mail", msg);
   try {
-    await sgMail.send(msg);
+    await transporter.sendMail(msg);
     console.log("mail sent");
   } catch (e) {
     console.error(e);
